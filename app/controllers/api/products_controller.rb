6b106e16a614a4eb06639d0f28bf7428cb50c167
Arnaldo Tayao Jr <arnaldotayaojr@gmail.com>
2017-05-30 09:47:34 +0800

    Adds error handling to product API

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -2,15 +2,31 @@ class Api::ProductsController < ApplicationController
   def index
     ActionController::Parameters.permit_all_parameters = true
     sort = params[:sort] == "price" ? params[:sort] : nil
-    filter = params[:filter]
+    filter = filter_for(params[:filter].to_hash) if params[:filter]
     products = Product.order(sort).where(filter)
 
     paginate json: products
   end
 
   def show
-    product = Product.find(params[:id])
+    begin
+      product = Product.find params[:id]
+    rescue ActiveRecord::RecordNotFound
+      product = Product.new
+      product.errors.add(:id, "Wrong ID provided")
+      render_error(product, 404) and return
+    end
 
     render json: product
+  end
+
+  private
+
+  def filter_for(filter)
+    if ["category", "price"].include? filter.keys[0]
+      filter
+    else
+      nil
+    end
   end
 end
